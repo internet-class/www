f63a00b337cf67d21d50da9f113c4dbe630e5710
Geoffrey Challen <challen@buffalo.edu>
2016-08-04 17:04:51 -0400

    More testing.

--- test/lessons/index.js
+++ test/lessons/index.js
@@ -1,8 +1,12 @@
 var metalsmith = require('metalsmith'),
+    async = require('async'),
+		fs = require('fs-extra'),
     path = require('path'),
     _ = require('underscore'),
     chai = require('chai'),
     powerAssert = require('power-assert'),
+    md5 = require('md5'),
+		deepcopy = require('deepcopy'),
 		common = require('../../lib/common.js'),
 		lessons = require('../../lib/lessons.js');
 
@@ -57,6 +61,47 @@ describe('lessons.js', function() {
 				}
 				assert(err.message.startsWith("Duplicate UUIDs."));
 				powerAssert.deepEqual(common.walkSync(path.join(src, 'src')), previousFiles);
+				done();
+			});
+	});
+	it('should fail on duplicate UUIDs', function (done) {
+		var src = path.join(__dirname, 'fixtures/duplicate');
+		
+		fs.removeSync(path.join(src, 'src/lessons/challen@buffalo.edu/02_why_internet'))
+		
+		var previousLessonHash;	
+		async.series([
+			function(callback) {
+				metalsmith(src)
+					.use(lessons())
+					.build(function (err, files) {
+						if (err) {
+							return done(err);
+						}
+						assert.pathExists(path.join(src, 'src', lessons.lessonIDsFilename));
+						previousLessonHash = md5(fs.readFileSync(path.join(src, 'src', lessons.lessonIDsFilename)));
+						callback();
+					});
+			},
+			function(callback) {
+				var previousFiles = common.walkSync(path.join(src, 'src'));
+				fs.copySync(path.join(src, 'src/lessons/challen@buffalo.edu/01_why_internet'),
+					path.join(src, 'src/lessons/challen@buffalo.edu/02_why_internet'));
+
+				metalsmith(src)
+					.use(lessons())
+					.build(function (err, files) {
+						if (!err) {
+							return done(new Error("should fail"));
+						}
+						assert(err.message.startsWith("Duplicate UUIDs."));
+						fs.removeSync(path.join(src, 'src/lessons/challen@buffalo.edu/02_why_internet'))
+						powerAssert.deepEqual(common.walkSync(path.join(src, 'src')), previousFiles);
+						assert(md5(fs.readFileSync(path.join(src, 'src', lessons.lessonIDsFilename))) == previousLessonHash);
+						callback();
+					});
+			}],
+			function () {
 				done();
 			});
 	});
