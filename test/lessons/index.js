c8f08074ed1482ca5426b4cd73b388b671aa910f
Geoffrey Challen <challen@buffalo.edu>
2016-08-08 13:03:44 -0400

    Lesson process testing.

--- test/lessons/index.js
+++ test/lessons/index.js
@@ -7,15 +7,41 @@ var metalsmith = require('metalsmith'),
     powerAssert = require('power-assert'),
     md5 = require('md5'),
 		deepcopy = require('deepcopy'),
+		temp = require('temp'),
 		common = require('../../lib/common.js'),
 		lessons = require('../../lib/lessons.js');
 
 chai.use(require('chai-fs'));
+temp.track();
 var assert = chai.assert;
+
+var metalsmithTempDir = function() {
+	var src = temp.mkdirSync();
+	fs.mkdirsSync(path.join(src, 'src'));
+	return src;
+}
+
+var copyFixture = function(src, base, dest) {
+	fs.mkdirsSync(path.dirname(path.join(base, 'src', dest)));
+	fs.copySync(path.join(__dirname, 'fixtures', src),
+			path.join(base, 'src', dest), { preserveTimestamps: true });
+	return;
+}
+
+var sameFile = function(base, src, prevHash) {
+	newHash = md5(fs.readFileSync(path.join(base, 'src', src)));
+	if (prevHash !== undefined) {
+		assert(newHash == prevHash, path.join(base, 'src', src) + ' has changed')
+	} else {
+		return newHash;
+	}
+}
 
 describe('lessons.js', function() {
 	it('should do nothing when there is nothing to do', function (done) {
-		var src = path.join(__dirname, 'fixtures/empty');
+		var src = metalsmithTempDir();
+		copyFixture('unmarked/01', src, '01');
+		copyFixture('unmarked/01', src, '02');
 		var previousFiles = common.walkSync(path.join(src, 'src'));
 		
 		metalsmith(src)
@@ -35,7 +61,9 @@ describe('lessons.js', function() {
 			});
 	});
 	it('should fail on nested lessons', function (done) {
-		var src = path.join(__dirname, 'fixtures/nested');
+		var src = metalsmithTempDir();
+		copyFixture('unmarked/01', src, 'lessons/i@i.me/01');
+		copyFixture('unmarked/01', src, 'lessons/i@i.me/01/01');
 		var previousFiles = common.walkSync(path.join(src, 'src'));
 		
 		metalsmith(src)
@@ -50,7 +78,9 @@ describe('lessons.js', function() {
 			});
 	});
 	it('should fail on new duplicate UUIDs', function (done) {
-		var src = path.join(__dirname, 'fixtures/newduplicate');
+		var src = metalsmithTempDir();
+		copyFixture('marked/01', src, 'lessons/i@i.me/01');
+		copyFixture('marked/01', src, 'lessons/i@i.me/02');
 		var previousFiles = common.walkSync(path.join(src, 'src'));
 		
 		metalsmith(src)
@@ -65,9 +95,9 @@ describe('lessons.js', function() {
 			});
 	});
 	it('should fail on duplicate UUIDs', function (done) {
-		var src = path.join(__dirname, 'fixtures/duplicate');
-		
-		fs.removeSync(path.join(src, 'src/lessons/challen@buffalo.edu/02_why_internet'))
+		var src = metalsmithTempDir();
+		copyFixture('unmarked/01', src, 'lessons/i@i.me/01');
+		var previousFiles = common.walkSync(path.join(src, 'src'));
 		
 		var previousLessonHash;	
 		async.series([
@@ -79,14 +109,14 @@ describe('lessons.js', function() {
 							return done(err);
 						}
 						assert.pathExists(path.join(src, 'src', lessons.lessonIDsFilename));
-						previousLessonHash = md5(fs.readFileSync(path.join(src, 'src', lessons.lessonIDsFilename)));
+						lessonHash = sameFile(src, lessons.lessonIDsFilename);
 						callback();
 					});
 			},
 			function(callback) {
+				fs.copySync(path.join(src, 'src/lessons/i@i.me/01'),
+					path.join(src, 'src/lessons/i@i.me/02'));
 				var previousFiles = common.walkSync(path.join(src, 'src'));
-				fs.copySync(path.join(src, 'src/lessons/challen@buffalo.edu/01_why_internet'),
-					path.join(src, 'src/lessons/challen@buffalo.edu/02_why_internet'));
 
 				metalsmith(src)
 					.use(lessons())
@@ -95,9 +125,8 @@ describe('lessons.js', function() {
 							return done(new Error("should fail"));
 						}
 						assert(err.message.startsWith("Duplicate UUIDs."));
-						fs.removeSync(path.join(src, 'src/lessons/challen@buffalo.edu/02_why_internet'))
 						powerAssert.deepEqual(common.walkSync(path.join(src, 'src')), previousFiles);
-						assert(md5(fs.readFileSync(path.join(src, 'src', lessons.lessonIDsFilename))) == previousLessonHash);
+						sameFile(src, lessons.lessonIDsFilename, lessonHash);
 						callback();
 					});
 			}],
