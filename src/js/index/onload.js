f4aa3565f4d0d9bc7fb8e9ea5aa90c2b099bcb18
Geoffrey Challen <challen@buffalo.edu>
2016-08-28 12:47:44 -0400

    Snazzy home page.

--- src/js/index/onload.js
+++ src/js/index/onload.js
@@ -12,22 +12,65 @@ $(function () {
       }
       $(this).css({ "font-size": textSize + "px" });
     });
-  }
+  };
   fullscreen();
   $(window).resize(function () {
     fullscreen();
   });
-  window.setTimeout(function () {
-    var video = $("video#background");
-    if ($(video).get(0).paused) {
+  var setPaused = function (paused) {
+    if (paused) {
       $("#play_button").css({ display: 'inline' });
       $("#pause_button").css({ display: 'none' });
     } else {
       $("#pause_button").css({ display: 'inline' });
       $("#play_button").css({ display: 'none' });
     }
-    $("#play_button, #pause_button").css({ visibility: 'visible' });
-  }, 100);
+  };
+  var playPause = function () {
+    var paused = $(this).attr('id') === 'pause';
+    var video = $("video#background").get(0);
+    if (paused) {
+      video.pause();
+    } else {
+      video.play();
+    }
+  };
+  var setMuted = function (muted) {
+    if (muted) {
+      $("#unmute_button").css({ display: 'inline' });
+      $("#mute_button").css({ display: 'none' });
+    } else {
+      $("#mute_button").css({ display: 'inline' });
+      $("#unmute_button").css({ display: 'none' });
+    }
+  };
+  var muteUnmute = function () {
+    var mute = $(this).attr('id') === 'mute';
+    var video = $("video#background").get(0);
+    if (mute) {
+      $(video).prop('muted', true);
+    } else {
+      $(video).prop('muted', false);
+    }
+    setMuted(mute);
+  };
+
+  $("#play, #pause").click(playPause);
+  $("#mute, #unmute").click(muteUnmute);
+
+  window.setTimeout(function () {
+    var video = $("video#background").get(0);
+    setPaused(video.paused);
+    video.addEventListener('play', function () {
+      setPaused(false);
+    }, false);
+    video.addEventListener('pause', function () {
+      setPaused(true);
+    }, false);
+    $("#play_button, #pause_button, #unmute_button, #mute_button").css({ visibility: 'visible' });
+
+  }, 50);
+
 });
 
 // vim: ts=2:sw=2:et
