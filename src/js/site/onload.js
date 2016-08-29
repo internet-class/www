b9ec2e87581e6f5dc85ed58f2a67fce9cfbbd2ec
Geoffrey Challen <challen@buffalo.edu>
2016-08-28 23:45:16 -0400

    Still going.

--- src/js/site/onload.js
+++ src/js/site/onload.js
@@ -5,6 +5,24 @@ $(function () {
   $('.toc-wrapper').pushpin({
     top: $('.toc-wrapper').offset().top
   });
+  
+  var footnotesActive = {};
+  var activateFootnote = function (elem) {
+    var uniqueId = $(elem).attr('id');
+    if (!(footnotesActive[uniqueId])) {
+      footnotesActive[uniqueId] = true;
+      Materialize.toast($(elem).data('content'), 5000);
+      window.setTimeout(function () {
+        delete(footnotesActive[uniqueId]);
+      }, 5000);
+    }
+  }
+  $('.footnote').click(function() {
+    activateFootnote(this);
+  });
+  $('.footnote').mouseenter(function() {
+    activateFootnote(this);
+  });
 });
 
 // vim: ts=2:sw=2:et
