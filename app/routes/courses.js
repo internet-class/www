921ca42261c4862c9d1c8c5b2a705562ce97de22
Geoffrey Challen <challen@buffalo.edu>
2016-09-04 13:43:09 -0400

    Simplest dynamic version working.

--- app/routes/courses.js
+++ app/routes/courses.js
@@ -9,7 +9,6 @@ var courses = {};
 var lessons = {};
 
 var routeCourses = function (coursesFile, lessonsFile) {
-
 	courses = jsonfile.readFileSync(coursesFile);
 	lessons = jsonfile.readFileSync(lessonsFile);
 	
@@ -17,18 +16,25 @@ var routeCourses = function (coursesFile, lessonsFile) {
 	_.each(courses.slug_to_uuid, function (uuid, slug) {
 		router.use('/' + slug, routeCourse(courses[uuid]));
 	});
+
 	return router;
 }
 
 var routeCourse = function (course) {
 	var router = express.Router();
 	router.get('/', function (req, res) {
-		videoIndex(course, req, res);
+		renderIndex(course, req, res);
 	});
+	_.each(course.lessons, function (lesson) {
+		router.get('/' + lesson.path, function (req, res) {
+			renderLesson(course, lesson, req, res);
+		});
+	});
+
 	return router;
 }
 
-var videoIndex = function (course, req, res) {
+var renderIndex = function (course, req, res) {
 	var lessonIndex = _.map(course.orderedLessons, function (lesson) {
 		var listLesson = lessons[lesson.uuid];
 		listLesson.firstVideo = listLesson.videos[0];
@@ -38,6 +44,26 @@ var videoIndex = function (course, req, res) {
 	res.render('index', {
 		course: course,
 		lessons: lessonIndex
+	});
+}
+
+var renderLesson = function (course, lesson, req, res) {
+	lesson = _.extend(lesson, lessons[lesson.uuid]);
+	var previous, next;
+	if (lesson.previous) {
+		previous = lessons[lesson.previous.uuid];
+		previous.path = lesson.previous.path;
+	}
+	if (lesson.next) {
+		next = lessons[lesson.next.uuid];
+		next.path = lesson.next.path;
+	}
+	console.log(lesson);
+	res.render('lesson', {
+		course: course,
+		lesson: lesson,
+		previous: previous,
+		next: next
 	});
 }
 
