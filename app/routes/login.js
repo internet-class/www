974b7988f4b6d60ec6e7503ad0dde6145e5da39d
Geoffrey Challen <challen@buffalo.edu>
2016-09-05 11:37:08 -0400

    Correct user initialization at startup.

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,24 +1,68 @@
 'use strict';
 
 var app = require('../app'),
+    assert = require('assert'),
 		express = require('express'),
-		passport = require('passport');
+		passport = require('passport'),
+    moment = require('moment');
+
+function returnToRedirect(req, res) {
+  if (req.query && req.query.returnTo) {
+    res.redirect(req.query.returnTo);
+  } else {
+    res.redirect('/');
+  }
+}
 
 var router = express.Router();
 router.get('/callback',
   passport.authenticate('auth0', { failureRedirect: '/login' }),
   function(req, res) {
-    console.log("Here");
     if (!req.user) {
       throw new Error('user null');
     }
     var users = app.get('db').collection('users');
-    console.log(req.user);
-    if (req.query && req.query.returnTo) {
-      res.redirect(req.query.returnTo);
-    } else {
-      res.redirect('/');
-    }
+    var update = users.updateOne({ 
+      _id: req.user.id,
+    }, {
+      $set: {
+        lastLogin: moment.utc().toDate()
+      }
+    }, {
+      upsert: true
+    });
+    update.then(function (result) {
+      assert(result.matchedCount == 1);
+      console.log(result);
+      if (result.upsertedCount == 1) {
+        var assignedCourse = req.user._json.user_metadata.assignedCourse;
+        assert(assignedCourse);
+        var firstLesson = app.get('courses').courses[assignedCourse].first_lesson;
+        assert(firstLesson);
+        console.log("There");
+
+        users.updateOne({
+          _id: req.user.id,
+        }, {
+          $set: {
+            courses: {
+              current: assignedCourse
+            },
+            lessons: {
+              current: [ firstLesson],
+              completed: []
+            }
+          }
+        }, function (err, result) {
+          assert(!err);
+          returnToRedirect(req, res);
+        });
+      } else {
+        returnToRedirect(req, res);
+      }
+    }).catch(function (err) {
+      throw(err);
+    });
   }, function (err, req, res, next) {
     if (err) {
       if (req.query && req.query.returnTo) {
