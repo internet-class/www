e096fce883e29d3e449d54f7cc93df7df8921209
Geoffrey Challen <challen@buffalo.edu>
2016-09-09 10:17:06 -0400

    Fix iFrame source problem. Thanks to Jonathan Loftus.

--- src/js/video/onload.js
+++ src/js/video/onload.js
@@ -38,6 +38,12 @@ var setChoice = function(choice) {
   });
 }
 
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
 var player;
 var videoInfo = {};
 function onYouTubeIframeAPIReady() {
